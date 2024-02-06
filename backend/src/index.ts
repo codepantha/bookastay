9febcce0677d6710454149b1d932270b0fcd8cb8
codepanther <pezechigozirim@gmail.com>
2024-02-06 15:04:32 +0100

    Implement user register api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,18 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import mongoose from 'mongoose';
+
+import userRoutes from './routes/users';
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors());
 
-app.get('/api/test', async(req: Request, res: Response) => {
-  res.json({ message: 'endpoint working!'})
-});
+app.use('/api/users', userRoutes);
 
 app.listen(3000, () => {
   console.log('app running on port 3000')
