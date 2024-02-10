9153e9f5e7372e2b83adff4473a8722685ccfa41
codepanther <pezechigozirim@gmail.com>
2024-02-10 11:03:58 +0100

    Create endpoint to add new hotel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,27 +3,40 @@ import cors from 'cors';
 import 'dotenv/config';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
+import path from 'path';
+import { v2 as cloudinary } from 'cloudinary';
 
 import userRoutes from './routes/users';
 import authRoutes from './routes/auth';
-import path from 'path';
+import myHotelRoutes from './routes/my-hotels';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-app.use(cors({
-  origin: [`${process.env.FRONTEND_URL}`, 'http://localhost:5173'],
-  credentials: true,
-}));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: [`${process.env.FRONTEND_URL}`, 'http://localhost:5173'],
+    credentials: true
+  })
+);
 
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.use('/api/users', userRoutes);
 app.use('/api/auth/', authRoutes);
+app.use('/api/my-hotels', myHotelRoutes);
 
 app.listen(3000, () => {
-  console.log('app running on port 3000')
-})
+  console.log('app running on port 3000');
+});
+
+export { cloudinary };
